661efdc02b96d614ff187075e7bebe3751f28eaa
Njaka <hei.herinjaka@gmail.com>
2025-09-03 11:43:48 +0300

    ajout du question 2

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from http.client import HTTPException
+from typing import List
+
+
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.requests import Request
@@ -10,4 +15,35 @@ app = FastAPI()
 @app.get("/ping")
 def hello():
     return Response(content="pong", status_code=200, media_type="text/plain")
+
+
+# 2 a
+class Characteristic(BaseModel):
+    max_speed: float
+    max_fuel_capacity: float
+
+class Car(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+cars_db = []
+
+@app.post("/cars", status_code=201)
+async def create_cars(cars: List[Car]):
+
+    try:
+
+        for car in cars:
+            cars_db.append(car.dict())
+
+        return {"message": f"Successfully created {len(cars)} car(s)", "cars": cars}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.get("/cars")
+async def get_cars():
+    return cars_db
+
+
 
